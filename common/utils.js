62445e3ed5f4b68dd7d5cbf5680a7f5f53ef9525
cficht <chris.ficht@gmail.com>
2020-01-30 10:03:34 -0800

    made list page that lists friend quests in links that link to details page with corresponding id

--- common/utils.js
+++ common/utils.js
@@ -16,3 +16,14 @@ export function userStore(userForm) {
 
     return userObject;
 }
+
+export function updateUserText(user) {
+    const userNameText = document.getElementById('user-name');
+    const userBdayText = document.getElementById('user-bday');
+    const userFriendsText = document.getElementById('user-friends');
+    const userLikesText = document.getElementById('user-likes');
+    userNameText.textContent = `Name: ${user.name}`;
+    userBdayText.textContent = `Birthday: ${user.birthday}`;
+    userFriendsText.textContent = `Friends: ${user.health}`;
+    userLikesText.textContent = `Likes: ${user.money}`;
+}
---
+++ list/list.js
@@ -0,0 +1,22 @@
+import friendArray from '../data/data.js';
+import { getUser, setUser } from '../data/storage.js';
+import { updateUserText } from '../common/utils.js';
+
+const friendList = document.getElementById('friend-list');
+const pageTitle = document.getElementById('page-title');
+
+const user = getUser();
+const friends = friendArray;
+
+updateUserText(user);
+
+pageTitle.textContent = `Facestagrammer: ${user.name}`;
+
+friends.forEach((friend) => {
+    const friendLi = document.createElement('li');
+    const friendURL = document.createElement('a');
+    friendURL.href = `../details/?id=${friend.id}`;
+    friendURL.textContent = friend.name;
+    friendLi.appendChild(friendURL);
+    friendList.appendChild(friendLi);
+});
